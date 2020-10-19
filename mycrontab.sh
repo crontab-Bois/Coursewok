49b0dc123685dea112441ea890b0bd2b26685748
harryaddlesee <harry@addlesee.co.uk>
2020-10-19 19:01:58 +0100

    selecting option 1

--- mycrontab.sh
+++ mycrontab.sh
@@ -4,4 +4,11 @@ echo "2. Insert a Job"
 echo "3. Edit a Job"
 echo "4. Remove a Job"
 echo "5. Remove all Jobs"
-echo "9. Exit"
+echo "9. Exit
+
+while true; do
+	read -r input
+	if ["$input" = "1"]; then
+		echo "1 selected"
+	fi
+done
