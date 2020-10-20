987a1fa8f45c7fe2a15e928728a3bb29de079118
harryaddlesee <harry@addlesee.co.uk>
2020-10-20 19:07:52 +0100

    Able to select options

--- mycrontab.sh
+++ mycrontab.sh
@@ -1,14 +1,32 @@
 #!/bin/bash
+
+while :
+do
+
 echo "1. Display Crontab Jobs"
 echo "2. Insert a Job"
 echo "3. Edit a Job"
 echo "4. Remove a Job"
 echo "5. Remove all Jobs"
 echo "9. Exit"
+echo "Please select an option."
 
-while true; do
-	read -r input
-	if ["$input" = "1"]; then
-		echo "1 selected"
-	fi
+read -r -n 1 selection
+printf "\n"
+case $selection in
+1* )	echo "Option 1 selected";;
+2* )	echo "Option 2 selected";;
+3* )	echo "Option 3 selected";;
+4* )	echo "Option 4 selected";;
+5* )	echo "Option 5 selected";;
+9* )	exit 0;;
+
+* )	echo "Invalid Option, try again";;
+esac
 done
+#while true; do
+	#read input
+	#if ["$input" = "1"]; then
+		#echo "1 selected"
+	#fi
+#done
